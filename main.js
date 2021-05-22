d13bd96fd14b19753f1e8b3b8d89d105e6d2aff7
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-05-21 19:04:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var i=0;
+var images=["","https://tse2.mm.bing.net/th?id=OIP.Sl4xAbUBSGnfpmvczmV4VgHaDW&pid=Api&P=0&w=439&h=199", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/A_Happy_Working_Mom_Cartoon.svg/1200px-A_Happy_Working_Mom_Cartoon.svg.png"];
+function MrMyFunction1() {
+document.getElementById("image1").src=images[i] ;
+
+var name=["Gurwinder Singh", "Agampaul Gill", "Paramjeet Kaur"]
+document.getElementById("nm1").innerHTML=name[i];
+ i++ };
